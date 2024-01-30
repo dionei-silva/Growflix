1702f3c7c6308f758e2b876168f8f613c61a4107
Dionei Silva <dionei.silva@gmail.com>
2024-01-30 14:47:43 -0300

    start project

---
+++ src/js/data.js
@@ -0,0 +1,9 @@
+// videos aonde um objeto com image title link categoria
+const movies =[
+  {
+  	img: '', //img.youtube so alreta apos .com/vi/'id'
+  	title: '', // titulo do video no yt
+  	link: '', // somente a url 
+  	category: '', // coloca a categoria
+},
+]
